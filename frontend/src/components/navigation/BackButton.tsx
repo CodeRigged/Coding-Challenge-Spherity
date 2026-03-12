c8713a1a122d66c1ce2fbc1eab8fd91f5999384f
CodeRigged <tantonovics@gmail.com>
2026-03-12 17:26:29 +0100

    feat(frontend): add BackButton-component

---
+++ frontend/src/components/navigation/BackButton.tsx
@@ -0,0 +1,13 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack"
+import { IconButton } from "@mui/material"
+import { useNavigate } from "react-router-dom"
+
+const BackButton = () => {
+  const navigate = useNavigate()
+  return (
+    <IconButton onClick={() => navigate(-1)} aria-label="Back">
+      <ArrowBackIcon />
+    </IconButton>
+  )
+}
+export default BackButton
